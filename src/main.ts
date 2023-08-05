9faca6c02eab65406dcbc73b50e98c174b03f362
LessThanZero000 <129956464+LessThanZero000@users.noreply.github.com>
2023-08-05 07:11:33 +0000

    Update python on AT next

--- src/main.ts
+++ src/main.ts
@@ -6,4 +6,4 @@ dotenv.config();
 async function main() {
   const textStream = await streamText({
     model: openai.CompletionTextGenerator({
-      model: "gpt-3.5-turbo-
+      model: "gpt-3.5-turbo-ins
